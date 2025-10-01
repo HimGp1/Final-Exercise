d139673ea5b7d7f04a3c4b0adf52652dffafba4d
HimGp1 <him.ahmad@student.forsbergsskola.se>
2025-10-01 14:36:49 +0200

    Moved Project

---
+++ Final Exercise/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Please give me a number of seconds.");
+int seconds = int.Parse(Console.ReadLine());
+float totalDays = (float)seconds / 86400;
+
+int days = seconds / (24*3600);
+
+seconds %= 24 * 3600; //get the remainder of seconds after removing number of days
+int hours = seconds / 3600; //one hour consists of 3600 seconds
+
+seconds %= 3600; //get the remaining seconds by removing the hours
+int minutes = seconds / 60;
+
+seconds %= 60;
+
+Console.WriteLine($"Seconds: {seconds}");
+Console.WriteLine($"Minutes: {minutes}");
+Console.WriteLine($"Hours: {hours}");
+Console.WriteLine($"Days: {days}");
+
+Console.WriteLine($"{days}.{hours}.{minutes}.{seconds}");
+
+Console.WriteLine($"In total that's {totalDays} days.");
